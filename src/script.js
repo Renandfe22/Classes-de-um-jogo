ae1b09b1e8b6ad7b8d4f309c073edd47fcef2b3c
renandfe22 <lealrenan@hotmail.com>
2023-11-16 21:48:44 -0300

    atualização3

--- src/script.js
+++ src/script.js
@@ -7,12 +7,12 @@ class Heroi {
     }
 
     ataque(inimigo) {
-        console.log(`O ${this.nome} sendo um ${this.classe} de ${this.idade} avançou contra o inimigo usando a ${this.arma} e causou 50 de dano.`);
+        adicionarMensagem(`O ${this.nome} sendo um ${this.classe} de ${this.idade} avançou contra o inimigo usando a ${this.arma} e causou 50 de dano.`);
         inimigo.dano(50);
     }
 
     dano(valor) {
-        console.log(`${this.nome} recebeu ${valor} de dano.`);
+        adicionarMensagem(`${this.nome} recebeu ${valor} de dano.`);
     }
 }
 
@@ -22,19 +22,26 @@ class Mago extends Heroi {
     }
 
     contraAtaque(inimigo) {
-        console.log(`${this.nome} o Mago contra-atacou ${inimigo.nome} o Guerreiro e causou 75 de dano.`);
+        adicionarMensagem(`${this.nome} o Mago contra-atacou ${inimigo.nome} o Guerreiro e causou 75 de dano.`);
         inimigo.dano(75);
     }
 
     revancheEspecial(inimigo) {
-        console.log(`${this.nome} o Mago foi desafiado para uma revanche especial por ${inimigo.nome} o Guerreiro.`);
-        console.log(`O ${inimigo.nome} o Guerreiro utilizou seu especial "Força Total" e causou 400 de dano.`);
-        console.log(`O ${this.nome} o Mago foi aniquilado pelo especial do ${inimigo.nome} o Guerreiro.`);
+        adicionarMensagem(`${this.nome} o Mago foi desafiado para uma revanche especial por ${inimigo.nome} o Guerreiro.`);
+        adicionarMensagem(`O ${inimigo.nome} o Guerreiro utilizou seu especial "Força Total" e causou 400 de dano.`);
+        adicionarMensagem(`O ${this.nome} o Mago foi aniquilado pelo especial do ${inimigo.nome} o Guerreiro.`);
     }
 }
 
 let guerreiro = new Heroi("carniceiro", "47 anos", "Guerreiro", "espada");
 let magoBapsa = new Mago("bapsa", "75 anos", "magia");
+
+function adicionarMensagem(mensagem) {
+    const dialogContainer = document.getElementById('dialog-container');
+    const mensagemElement = document.createElement('p');
+    mensagemElement.textContent = mensagem;
+    dialogContainer.appendChild(mensagemElement);
+}
 
 guerreiro.ataque(magoBapsa);
 magoBapsa.contraAtaque(guerreiro);
